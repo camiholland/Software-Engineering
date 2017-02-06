7d4006b9286a1a0602e6cc0642828e6ea6069096
kwc12 <kwc12@users.noreply.github.com>
2017-02-06 14:00:00 -0500

    Add files via upload

---
+++ TrackToCircuitInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trackmodel;
+/**
+ *
+ * @author Kevin Carr
+ */
+public interface TrackToCircuitInterface extends TrackModelInterface{
+    public int getSpeedLimit(int block);
+    public int getSetPointSpeed();
+    public int getSetAuthority();
+    public boolean isTrainPresent(int block);
+}
---
+++ TrackToWaysideInterface.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trackmodel;
+
+/**
+ *
+ * @author kwc12
+ */
+public interface TrackToWaysideInterface extends TrackToCircuitInterface{
+    //Method will set speed that can be sent to track circuit
+	public boolean setSpeed(int speed);
+	//Method that will set the authority (distance) the train is able to travel to track circuit
+	public boolean setAuthority(int dist);
+	
+	/*
+	 *The settings of the track lights will be decided by the wayside controller and can be changed
+	 *using this method.
+	 *
+	 *@param line		Red/Green line 
+	 *@param section	Letter indicating the section of track light is found on.
+	 *@param block		Block number for more specific location (might not be necessary)
+	 *@param direction	True means alphabetical order, False means reverse alphabetical, check map for reference
+	 *@param light		True for green (open), false for red (closed)
+	 *@return			True, if light succesfully changes. False, if no change occurs.
+	*/
+	public boolean setLights(char line, char section, int block, boolean direction, boolean light);
+	
+	/*
+	 *The railroad crossing can be turned on or off with this method.
+	 *
+	 *@param open	True for closing of the road to let train pass, false to open crossing for cars.
+	 *@return		True if succesfully changed, false otherwise.
+	 */
+	public boolean setCrossing(boolean open);
+	
+}
