b4aa432876a9061ed8fb730c6b075ae25a884c4e
obkeel <obkeel@150.212.28.127>
2017-04-13 16:46:42 -0400

    updated System

--- System/src/com/wonderfresh/systemwrapper/SystemWrapper.java
+++ System/src/com/wonderfresh/systemwrapper/SystemWrapper.java
@@ -25,6 +25,7 @@ import com.wonderfresh.interfaces.Interfaces;
 import com.wonderfresh.interfaces.TrainControllerInterfaceImpl;
 import com.wonderfresh.interfaces.TrainModelAPIImpl;
 import com.wonderfresh.interfaces.TrackModelInterface;
+import com.wonderfresh.trackcontroller.TrackController;
 
 
 public class SystemWrapper {
@@ -34,6 +35,7 @@ public class SystemWrapper {
     public static Train trainModels;
     public static Mbo mbo;
     public static TrackSimulator trackSimulator;
+    public static TrackController trackController;
     
     
     /**
@@ -50,7 +52,7 @@ public class SystemWrapper {
         trainModels = new Train();
         mbo = new Mbo();
         mbo.start();
-        
+        trackController = new TrackController();
         //trackSimulator.start();
         
         Interfaces.setTrainControllerInterface(new TrainControllerInterfaceImpl(trainControllers));
@@ -66,6 +68,8 @@ public class SystemWrapper {
                 (new SystemLauncher(trainControllers, trainModels,mbo)).setVisible(true);
             }
         });
+        
+        
     }
     
 }
